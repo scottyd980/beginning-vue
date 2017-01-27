0d842ae801b3e718f1c8b544ff42af9a4e7255b4
Scott Douglass <scott@scottyd.net>
2017-01-26 19:48:50 -0500

    Added a todo item component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <ol>
+      <todo-item v-for="item in groceryList" v-bind:todo="item"></todo-item>
+    </ol>
     <hello></hello>
   </div>
 </template>
 
 <script>
 import Hello from './components/Hello'
+import TodoItem from './components/TodoItem'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    Hello,
+    TodoItem
+  },
+  data () {
+    return {
+      groceryList: [
+        { text: 'Vegetables' },
+        { text: 'Cheese' },
+        { text: 'Whatever else humans are supposed to eat' }
+      ]
+    }
   }
 }
 </script>
